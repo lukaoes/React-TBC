f77754b7192b5a703b1f2ed998e82bae3b658955
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-03-08 14:48:37 +0400

    feat: Add responsive header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import './App.css';
+import './App.scss';
 
 function App() {
+  let openBurger = false;
+
+  const toggleBurgerMenu = () => {
+    openBurger = !openBurger;
+
+    const burgerMenu = document.getElementById("burger");
+    const menu = document.getElementById("menu");
+
+    if (burgerMenu && menu) {
+      burgerMenu.classList.toggle("is-active", openBurger);
+      menu.classList.toggle("is-active", openBurger);
+    }
+  };
+
   return (
     <div>
+      <header className="header">
+        <nav className="navbar container">
+            <a href="/" className="brand">LUMART</a>
+            <div 
+              className={`burger ${openBurger ? 'is-active' : ''}`} 
+              id="burger" 
+              onClick={() => toggleBurgerMenu()}
+            >
+              <span className="burger-line"></span>
+              <span className="burger-line"></span>
+              <span className="burger-line"></span>
+            </div>
+            <div className={`menu ${openBurger ? 'is-active' : ''}`} id="menu">
+              <ul className="menu-inner">
+                  <li className="menu-item"><a href="/" className="menu-link">Home</a></li>
+                  <li className="menu-item"><a href="/" className="menu-link">Products</a></li>
+                  <li className="menu-item"><a href="/" className="menu-link">About</a></li>
+                  <li className="menu-item"><a href="/" className="menu-link">Contact</a></li>
+              </ul>
+            </div>
+            <a href="/" className="menu-block">REGISTER</a>
+        </nav>
+      </header>
     </div>
   );
 }
