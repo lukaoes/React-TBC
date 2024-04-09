194da2868beea0f9f92ab661f67bf6c187822992
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-04-09 20:55:41 +0400

    Make: Blog post page usable

--- app/blog/[blogPost]/page.js
+++ app/blog/[blogPost]/page.js
@@ -1,18 +1,30 @@
+'use client'
 import Image from 'next/image'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const BlogPost = ({ params: {blogPost} }) => {
+  const [singleBlog, setSingleBlog] = useState([])
+  useEffect(() => {
+    axios.get(`https://dummyjson.com/posts/${blogPost}`).then(res => {
+      setSingleBlog(res.data)
+    })
+  }, [])
+
   return (
     <div>
       <div className="px-[20px] py-[60px] w-full bg-[#fffad1] mb-[270px] md:flex md:flex-row md:items-center md:justify-between">
         <div className="max-w-[1200px] mx-[auto] my-[0] relative">
-          <span className="block px-[15px] py-[5px] tracking-[1px] border-[2px] border-[solid] border-[#000] font-semibold rounded-[20px] w-[170px] mx-[auto] my-[0] text-center mb-[20px]">
-            PROGRAMMING
+        {singleBlog.tags && singleBlog.tags.length > 0 && (
+          <span className="block uppercase px-[15px] py-[5px] tracking-[1px] border-[2px] border-[solid] border-[#000] font-semibold rounded-[20px] w-[170px] mx-[auto] my-[0] text-center mb-[20px]">
+            {singleBlog.tags[0]}
           </span>
+        )}
           <h1 className="font-semibold text-center mb-[30px]">
-            A Programmers Guide to Battling Imposter Syndrome
+            {singleBlog.title}
           </h1>
-          <p className="text-center font-light text-[18px] mb-[30px]"> 
-            How to boost self-preception through communication, writing and kinship.
+          <p className="text-center font-light text-[18px] mb-[30px] w-full"> 
+            {singleBlog.body}
           </p>
           <Image
             className="w-[90%] absolute left-1/2 transform -translate-x-1/2 h-[300px] object-cover md:h-[400px]"
@@ -27,35 +39,23 @@ const BlogPost = ({ params: {blogPost} }) => {
         <div>
           <h2 className='text-[22px] mb-[15px] md:text-2xl md:mb-4'>Introduction</h2>
           <p className='mb-[25px] md:mb-6 md:text-base'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quaerat omnis eius beatae nam iusto accusantium 
-            consectetur suscipit voluptatum eligendi. Alias reiciendis 
-            corrupti dolores! Cum fugiat nihil error praesentium, 
-            laudantium inventore?
+            {singleBlog.body}
           </p>
           <h2 className='text-[22px] mb-[15px] md:text-2xl md:mb-4'>Software and Tools</h2>
           <p className='mb-[25px] md:mb-6 md:text-base'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quaerat omnis eius beatae nam iusto accusantium 
-            consectetur suscipit voluptatum eligendi. Alias reiciendis 
-            corrupti dolores! Cum fugiat nihil error praesentium, 
-            laudantium inventor
+            {singleBlog.body}
           </p>
           <h2 className='text-[22px] mb-[15px] md:text-2xl md:mb-4'>Other resources</h2>
           <p className='mb-[25px] md:mb-6 md:text-base'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quaerat omnis eius beatae nam iusto accusantium 
-            consectetur suscipit voluptatum eligendi. Alias reiciendis 
-            corrupti dolores! Cum fugiat nihil error praesentium, 
-            laudantium inventor
+            {singleBlog.body}
           </p>
         </div>
         <div className='order-[-1] text-center mb-[15px] md:order-1 md:w-full md:text-center md:mb-4'>
-          <h3 className='mx-[0] my-[15px]'>Table of contents</h3>
+          <h3 className='mx-[0] my-[15px] font-medium	'>Table of contents</h3>
           <span className='block cursor-pointer mb-[8px] hover:underline'>Introduction</span>
           <span className='block cursor-pointer mb-[8px] hover:underline'>Software And Tools</span>
           <span className='block cursor-pointer mb-[8px] hover:underline'>Other Resources</span>
-          <h3 className='mx-[0] my-[15px]'>Author & Date</h3>
+          <h3 className='mx-[0] my-[15px] font-medium	'>Author & Date</h3>
           <div className="blog-post-author">
             <div className='flex items-center justify-center text-left gap-[10px] font-medium'>
               <Image
