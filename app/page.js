b947a40f55a42cc15e1cdbf4d04cbf99139320a9
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-04-10 23:11:13 +0400

    Add: Loading to main page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
 'use client'
+import FeaturedProducts from '@/components/Home/featuredProducts';
 import SearchBar from '@/components/Home/searchBar';
-import Card from '@/components/cards/Card';
 import axios from 'axios';
-import { Suspense, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Loading from './loading';
 const API = 'https://dummyjson.com/products'
+
+const DynamicFeaturedProducts = dynamic(() => import('@/components/Home/featuredProducts'), {
+  ssr: false,
+  loading: <Loading />,
+})
 
 export default function Home() {
   const [products, setProducts] = useState([])
   const [filteredCardData, setFilteredCardData] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   const handleSearch = (filteredData) => {
     setFilteredCardData(filteredData);
@@ -20,7 +24,6 @@ export default function Home() {
       .then((res) => {
         setProducts(res.data.products);
         setFilteredCardData(res.data.products);
-        setLoading(false)
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
@@ -34,17 +37,7 @@ export default function Home() {
         <div className="main-container">
           <h1 className="title">Featured Products</h1>
           <div className="featured-products">
-            <Suspense fallback={<Loading />}> 
-              {loading ? <Loading /> : (
-                <>
-                  {filteredCardData.length > 0 ? (
-                    <Card cardData={filteredCardData} />
-                  ) : (
-                    <p style={{ fontSize: '18px', fontWeight: '700'}}>No items found :(</p>
-                  )}
-                </>
-              )}
-            </Suspense>
+            <DynamicFeaturedProducts filteredCardData={filteredCardData} />
           </div>
         </div>
       </main>
