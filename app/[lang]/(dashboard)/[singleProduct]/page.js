f216ffddd3aa59634d428b64737372e3e3e82e49
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-04-20 21:21:25 +0400

    Add generateStaticParams to dynamic pages

--- app/[lang]/(dashboard)/[singleProduct]/page.js
+++ app/[lang]/(dashboard)/[singleProduct]/page.js
@@ -1,5 +1,16 @@
 import SingleProdInfo from "@/components/SingleProduct/singleProdInfo"
 
+export async function generateStaticParams() {
+  const response = await fetch('https://dummyjson.com/products/');
+  const products = await response.json();
+
+  const paths = products.products.map((post) => ({
+    params: { singleProduct: post.id }
+  }))
+
+  return paths
+}
+
 export default async function SingleProduct({ params }) {
   const {lang} = params
   const {singleProduct} = params
--- app/[lang]/(dashboard)/blog/[blogPost]/page.js
+++ app/[lang]/(dashboard)/blog/[blogPost]/page.js
@@ -1,6 +1,17 @@
 import { getDictionary } from '@/app/[lang]/dictionaries';
 import Image from 'next/image'
 
+export async function generateStaticParams() {
+  const response = await fetch('https://dummyjson.com/posts');
+  const posts = await response.json();
+
+  const paths = posts.posts.map((post) => ({
+    params: { blogPost: post.id }
+  }))
+
+  return paths
+}
+
 async function fetchPosts(blogPost) {
   const response = await fetch(`https://dummyjson.com/posts/${blogPost}`)
   const posts = await response.json();
