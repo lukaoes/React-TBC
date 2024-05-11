309f7e3839bc8bdccf61c66de525e20c278babc8
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-05-12 00:23:20 +0400

    add cart total component

--- app/[locale]/(dashboard)/cart/page.tsx
+++ app/[locale]/(dashboard)/cart/page.tsx
@@ -1,15 +1,18 @@
 import CartCard from "../../../../components/Cart/cartCard";
+import CartTotal from "../../../../components/Cart/cartTotal";
 
 const Cart = () => {
   return (
     <div className="container cart-layout">
       <h1>შენს კალათაში 2 ნივთია</h1>
-      <div>
+      <div className="cart-details-layout">
         <div>
           <CartCard />
           <CartCard />
         </div>
-        <div></div>
+        <div>
+          <CartTotal />
+        </div>
       </div>
     </div>
   );
