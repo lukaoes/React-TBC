2f10ddd229fbd13afa88c2521484e903b50304ac
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-05-17 18:56:57 +0400

    make admin page refresh and load users

--- components/Admin/adminNav.tsx
+++ components/Admin/adminNav.tsx
@@ -1,17 +1,35 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const AdminNav = () => {
-  const path = usePathname()
+  const path = usePathname();
+
+  const handleUsersClick = (e: { preventDefault: () => void }) => {
+    if (path === "/admin") {
+      e.preventDefault();
+      window.location.reload();
+    }
+  };
 
   return (
     <div className="flex justify-center">
-      <Link href="/admin" className={path === '/admin' ? 'active bg-[#2980b9] font-bold' : ''}>Users</Link>
-      <Link href="/admin/add" className={path === '/admin/add' ? 'active bg-[#27ae60] font-bold' : ''}>Add</Link>
+      <Link
+        href="/admin"
+        className={path === "/admin" ? "active bg-[#2980b9] font-bold" : ""}
+        onClick={handleUsersClick}
+      >
+        Users
+      </Link>
+      <Link
+        href="/admin/add"
+        className={path === "/admin/add" ? "active bg-[#27ae60] font-bold" : ""}
+      >
+        Add
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default AdminNav
+export default AdminNav;
