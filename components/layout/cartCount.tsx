b6de7013f14176810f68f8195411cc56699c7223
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-05-22 00:03:49 +0400

    Add cart count on icon

--- components/layout/cartCount.tsx
+++ components/layout/cartCount.tsx
@@ -1,9 +1,55 @@
 "use client";
-import { useAppContext } from "../../context";
+import { useEffect, useState } from "react";
+// import { useAppContext } from "../../context";
+import { getUserCart } from "../../api";
 
 const CartCount = () => {
-  const { state } = useAppContext();
-  return <span>{state ? state : 0}</span>;
+  const userId = 2;
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    const fetchCartCount = async () => {
+      try {
+        const cart = await getUserCart(userId);
+        const productQuantities = cart.products;
+        const count = Object.values<number>(productQuantities).reduce(
+          (acc, quantity) => acc + quantity,
+          0
+        );
+        setTotalCount(count);
+      } catch (error) {
+        console.error("Error fetching cart count:", error);
+      }
+    };
+
+    fetchCartCount();
+  }, []);
+
+  useEffect(() => {
+    const fetchCartCount = async () => {
+      try {
+        const cart = await getUserCart(userId);
+        const productQuantities = cart.products;
+        const count = Object.values<number>(productQuantities).reduce(
+          (acc, quantity) => acc + quantity,
+          0
+        );
+        setTotalCount(count);
+      } catch (error) {
+        console.error("Error fetching cart count:", error);
+      }
+    };
+
+    fetchCartCount();
+  }, [totalCount]);
+
+  // const { state } = useAppContext();
+  return (
+    <span>
+      {totalCount}
+      {/* <span>{state ? state : 0}</span> */}
+    </span>
+  );
 };
 
 export default CartCount;
--- components/layout/cartIcon.tsx
+++ components/layout/cartIcon.tsx
@@ -2,12 +2,23 @@ import Link from "next/link";
 import CartCount from "./cartCount";
 import Cart from "../../public/assets/images/cart.svg";
 import Image from "next/image";
+// import { getUserCart } from "../../api";
 
-const CartIcon = () => {
+const CartIcon = async () => {
+  // const userId = 2;
+  // const cart = await getUserCart(userId);
+  // const productQuantities = cart.products;
+
+  // const totalCount = Object.values<number>(productQuantities).reduce(
+  //   (acc, quantity) => acc + quantity,
+  //   0
+  // );
+
   return (
     <Link href="/cart" className="cart-icon">
       <Image src={Cart} alt="cart" width={20} height={20} />
       <CartCount />
+      {/* <span>{totalCount}</span> */}
     </Link>
   );
 };
