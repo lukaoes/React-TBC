8b538d7ec687cd5e06900b1102844d69776fe17c
budagovi <lukabudagovi@gmail.com>
2024-05-25 17:31:23 +0400

    added redirect on profile page for unauthorized users

--- app/[locale]/(dashboard)/profile/page.tsx
+++ app/[locale]/(dashboard)/profile/page.tsx
@@ -1,6 +1,9 @@
 import { setStaticParamsLocale } from "next-international/server";
 import ProfileLayout from "../../../../components/Profile/profileLayout";
 import { getStaticParams } from "../../../../locales/server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { AUTH_COOKIE_KEY } from "../../../../constants";
 
 export function generateStaticParams() {
   return getStaticParams()
@@ -8,6 +11,15 @@ export function generateStaticParams() {
 
 
 export default async function Profile({ params: { locale } }: { params: { locale: string } }) {
+
+  // redirect to homepage if user is unauthorized
+  const cookieStore = cookies();
+  const cookie = cookieStore.get(AUTH_COOKIE_KEY);
+
+  if (!cookie) {
+    redirect("/");
+  }
+
   setStaticParamsLocale(locale)
   return <ProfileLayout />
 }
