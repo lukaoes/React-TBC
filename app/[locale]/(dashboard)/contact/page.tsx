21236a5abfd5649f465ff1a728da17e8d100376e
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-02 13:35:50 +0400

    add second header to contact page

--- app/[locale]/(dashboard)/contact/page.tsx
+++ app/[locale]/(dashboard)/contact/page.tsx
@@ -1,8 +1,12 @@
 import ContactForm from "../../../../components/Contact/contactForm";
 import ContactInfo from "../../../../components/Contact/contactInfo";
+import SecondHeader from "../../../../components/layout/secondHeader";
+import contact from "../../../../public/assets/images/secondHeader/contact.jpg";
 const Contact = () => {
   return (
     <>
+      <SecondHeader title={"კონტაქტი"} backgroundImage={contact} />
+
       <div className="contact-layout">
         <ContactInfo />
         <ContactForm />
