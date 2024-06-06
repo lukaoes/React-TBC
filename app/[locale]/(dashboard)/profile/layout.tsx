44e97e490bc567ceada9aafa79b6631f443dc7df
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-06 13:28:36 +0400

    Make ui for pages that are not accessible without login

--- app/[locale]/(dashboard)/profile/layout.tsx
+++ app/[locale]/(dashboard)/profile/layout.tsx
@@ -1,14 +1,26 @@
+import { getSession } from "@auth0/nextjs-auth0";
 import ProfileUser from "../../../../components/Profile/profileUser";
+import LoginToAccess from "../../../../components/noAccess/loginToAccess";
 
-export default function ProfileLayout({
+export default async function ProfileLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="profile-container">
-      <ProfileUser />
-      {children}
-    </div>
-  );
+  try {
+    const session = await getSession();
+
+    if (!session || !session.user || !session.user.sub) {
+      return <LoginToAccess />;
+    }
+
+    return (
+      <div className="profile-container">
+        <ProfileUser />
+        {children}
+      </div>
+    );
+  } catch (error) {
+    return <div>მოგვიანებით სცადეთ.</div>;
+  }
 }
