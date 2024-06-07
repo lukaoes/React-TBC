a328da2bf45aece27ac93272db7fbda9e1bbd3d8
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-07 18:19:53 +0400

    fetch blog posts from new to old

--- app/api/blog/get-blog-display/route.ts
+++ app/api/blog/get-blog-display/route.ts
@@ -6,14 +6,16 @@ export const revalidate = 0;
 export async function GET() {
   try {
     const users = await sql`SELECT 
-    id,
-    user_id,
-    category,
-    main_photo,
-    title,
-    created_at
+          id,
+          user_id,
+          category,
+          main_photo,
+          title,
+          created_at
       FROM 
-    user_blogs;`;
+          user_blogs
+      ORDER BY 
+          id DESC;`;
     return NextResponse.json({ users }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
