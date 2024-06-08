d5b2b8b86f58a0359a70b1de767a17903275049f
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-08 18:43:56 +0400

    fix remove message issue and add min height on main element

--- app/api/contact/delete-message/[id]/route.ts
+++ app/api/contact/delete-message/[id]/route.ts
@@ -1,18 +1,20 @@
-import { sql } from '@vercel/postgres';
-import { NextRequest, NextResponse } from 'next/server';
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(request: NextRequest) {
-  const id = request.nextUrl.pathname.replace('/api/contact/delete-message', '');
+  const id = request.nextUrl.pathname.replace(
+    "/api/contact/delete-message/",
+    ""
+  );
 
   try {
-    if (!id) throw new Error('ID is required');
+    if (!id) throw new Error("ID is required");
 
     await sql`DELETE FROM contact WHERE id = ${Number(id)};`;
+
+    const users = await sql`SELECT * FROM contact;`;
+    return NextResponse.json({ users }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error: error }, { status: 500 });
   }
-
-  const users = await sql`SELECT * FROM users;`;
-
-  return NextResponse.json({ users }, { status: 200 });
 }
