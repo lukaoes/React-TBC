edd975f72762997f8a71aed71ba769ad8465b028
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-08 19:38:43 +0400

    add products on admin page and make it editable

---
+++ app/[locale]/(dashboard)/admin/products/page.tsx
@@ -0,0 +1,14 @@
+import AdminProductsTable from "../../../../../components/Admin/adminProductsTable";
+
+const AdminProducts = () => {
+  return (
+    <div>
+      <div className="admin-inbox">
+        <h1>ყველა პროდუქტი</h1>
+        <AdminProductsTable />
+      </div>
+    </div>
+  );
+};
+
+export default AdminProducts;
---
+++ components/Admin/adminProductsTable.tsx
@@ -0,0 +1,97 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { deleteSingleProduct, getUserProductsDisplay } from "../../actions";
+import { ProductsDisplay } from "../../types";
+import SingleProdEditModal from "../SingleProductPage/singleProdEditModal";
+import { getSingleProduct } from "../../actions";
+
+const AdminProductsTable = () => {
+  const [displayProds, setDisplayProds] = useState<ProductsDisplay[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] =
+    useState<ProductsDisplay | null>(null);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getUserProductsDisplay();
+      setDisplayProds(products);
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleEditClick = async (id: number) => {
+    const product = await getSingleProduct(id.toString());
+    setSelectedProduct(product);
+    setIsModalOpen(true);
+  };
+
+  const handleDeleteClick = async (id: number) => {
+    try {
+      await deleteSingleProduct(id);
+      // setDisplayProds((prevProds) =>
+      //   prevProds.filter((prod) => prod.id !== id)
+      // );
+    } catch (error) {
+      console.error("Failed to delete product:", error);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedProduct(null);
+  };
+
+  const handleSave = (updatedProduct: ProductsDisplay) => {
+    setDisplayProds((prevProds) =>
+      prevProds.map((prod) =>
+        prod.id === updatedProduct.id ? updatedProduct : prod
+      )
+    );
+    handleCloseModal();
+  };
+
+  return (
+    <div className="admin-table">
+      <div className="row admin-table-header blue">
+        <div className="cell">ID</div>
+        <div className="cell">სახელი</div>
+        <div className="cell">კატეგორია</div>
+        <div className="cell">მდებარეობა</div>
+        <div className="cell">edit</div>
+        <div className="cell">წაშლა</div>
+      </div>
+      {displayProds.map((product, index) => (
+        <div className="row" key={`product-admin-${index}`}>
+          <div className="cell" data-title="ID">
+            {product.id}
+          </div>
+          <div className="cell" data-title="სახელი">
+            {product.title_ge}
+          </div>
+          <div className="cell" data-title="კატეგორია">
+            {product.category}
+          </div>
+          <div className="cell" data-title="მდებარეობა">
+            {product.location}
+          </div>
+          <div className="cell" data-title="edit">
+            <button onClick={() => handleEditClick(product.id)}>edit</button>
+          </div>
+          <div className="cell" data-title="წაშლა">
+            <button onClick={() => handleDeleteClick(product.id)}>del</button>
+          </div>
+        </div>
+      ))}
+      {isModalOpen && selectedProduct && (
+        <SingleProdEditModal
+          // @ts-ignore
+          product={selectedProduct[0]}
+          onClose={handleCloseModal}
+          onSave={handleSave}
+        />
+      )}
+    </div>
+  );
+};
+
+export default AdminProductsTable;
