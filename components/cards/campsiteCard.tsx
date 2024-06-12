d5c6a107720de8477ff1c621ab4b714ca164aac1
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-12 12:49:56 +0400

    Display rating on campsite card

--- components/cards/campsiteCard.tsx
+++ components/cards/campsiteCard.tsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
-import { CampsitesDisplay } from "../../types";
+import { CampsitesDisplay, Review } from "../../types";
 import { FC } from "react";
+import { getCampReviews } from "../../actions";
 
 interface ICamps {
   camp: CampsitesDisplay;
 }
 
-const CampsiteCard: FC<ICamps> = ({ camp }) => {
+const CampsiteCard: FC<ICamps> = async ({ camp }) => {
+  const review = await getCampReviews(String(camp.id));
+
+  const calculateSatisfactionPercentage = (review: Review[]): number => {
+    const totalReviews = review.length;
+    const recommendedReviews = review.filter(
+      (review) => review.recommended
+    ).length;
+    return (recommendedReviews / totalReviews) * 100;
+  };
+
+  const satisfactionPercentage = calculateSatisfactionPercentage(review);
+  const reviewCount = review.length;
   return (
     <div className="campsite-card-layout">
       <Link href={`campsites/${camp.id}`} className="campsite-image-container">
@@ -37,7 +50,8 @@ const CampsiteCard: FC<ICamps> = ({ camp }) => {
           >
             <path d="M104 224H24c-13.255 0-24 10.745-24 24v240c0 13.255 10.745 24 24 24h80c13.255 0 24-10.745 24-24V248c0-13.255-10.745-24-24-24zM64 472c-13.255 0-24-10.745-24-24s10.745-24 24-24 24 10.745 24 24-10.745 24-24 24zM384 81.452c0 42.416-25.97 66.208-33.277 94.548h101.723c33.397 0 59.397 27.746 59.553 58.098.084 17.938-7.546 37.249-19.439 49.197l-.11.11c9.836 23.337 8.237 56.037-9.308 79.469 8.681 25.895-.069 57.704-16.382 74.757 4.298 17.598 2.244 32.575-6.148 44.632C440.202 511.587 389.616 512 346.839 512l-2.845-.001c-48.287-.017-87.806-17.598-119.56-31.725-15.957-7.099-36.821-15.887-52.651-16.178-6.54-.12-11.783-5.457-11.783-11.998v-213.77c0-3.2 1.282-6.271 3.558-8.521 39.614-39.144 56.648-80.587 89.117-113.111 14.804-14.832 20.188-37.236 25.393-58.902C282.515 39.293 291.817 0 312 0c24 0 72 8 72 81.452z"></path>
           </svg>
-          93% <span>(144)</span>
+          {satisfactionPercentage ? satisfactionPercentage.toFixed(0) : "100"}%{" "}
+          <span>({reviewCount})</span>
         </div>
         <Link href={`campsites/`}>
           <h3>{camp.name}</h3>
