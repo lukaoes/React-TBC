a11ebd56689600cd922f3bae1f09ac068e279683
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-13 11:25:14 +0400

    Make some pages unaccessible for unauthorised users

--- locales/client.ts
+++ locales/client.ts
@@ -1,7 +1,13 @@
-"use client"
-import { createI18nClient } from 'next-international/client'
- 
-export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale, } = createI18nClient({
-  en: () => import('./en'),
-  ge: () => import('./ge')
-})
+"use client";
+import { createI18nClient } from "next-international/client";
+
+export const {
+  useI18n,
+  useScopedI18n,
+  I18nProviderClient,
+  useChangeLocale,
+  useCurrentLocale,
+} = createI18nClient({
+  en: () => import("./en"),
+  ge: () => import("./ge"),
+});
