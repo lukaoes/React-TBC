fa3ce479bd2e3c2f12820b3ae6bec741ce37133e
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-15 12:07:20 +0400

    Fetch products by added time

--- app/api/products/get-user-products-display/route.ts
+++ app/api/products/get-user-products-display/route.ts
@@ -5,27 +5,31 @@ export const revalidate = 0;
 
 export async function GET() {
   try {
-    const users = await sql`SELECT 
-    id,
-    type,
-    category,
-    subcategory,
-    shoe_size,
-    clothing_size,
-    backpack_capacity,
-    tent_capacity,
-    main_photo,
-    title_ge,
-    description_ge,
-    title_en,
-    description_en,
-    price,
-    negotiable,
-    location,
-    created_at,
-    condition
+    const users = await sql`
+      SELECT 
+        id,
+        type,
+        category,
+        subcategory,
+        shoe_size,
+        clothing_size,
+        backpack_capacity,
+        tent_capacity,
+        main_photo,
+        title_ge,
+        description_ge,
+        title_en,
+        description_en,
+        price,
+        negotiable,
+        location,
+        created_at,
+        condition
       FROM 
-    user_products;`;
+        user_products
+      ORDER BY 
+        created_at DESC;
+    `;
     return NextResponse.json({ users }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
