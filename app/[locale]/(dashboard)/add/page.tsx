4beeded55c1ffe4fdab55507e407ff2bff08221f
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-17 22:30:29 +0400

    translate add page

--- app/[locale]/(dashboard)/add/page.tsx
+++ app/[locale]/(dashboard)/add/page.tsx
@@ -2,20 +2,17 @@ import Link from "next/link";
 import camping from "../../../../public/assets/images/camping.png";
 import shopping from "../../../../public/assets/images/shopping.png";
 import Image from "next/image";
+import { getScopedI18n } from "../../../../locales/server";
 
-const Add = () => {
+const Add = async () => {
+  const t = await getScopedI18n("add");
   return (
     <div className="add-layout">
       <div className="add-container">
         <Link href="/add/product">
           <div>
-            <h2>პროდუქტის გაყიდვა ან გაქირავება</h2>
-            <p>
-              გაყიდე ან გააქირავე საკუთარი პროდუქცია. თუ გაქვთ ბუნებასთან
-              კავშირში მქონე ნივთები, ტანსაცმელი, ფეხსაცმელი, აღჭურვილობა და
-              ა.შ. შეგიძლიათ განათავსოთ ჩვენს ვებ-გვერდზე გასაყიდათ, ან
-              გააქირაოთ თქვენი ნივთები დღიურად.
-            </p>
+            <h2>{t("product")}</h2>
+            <p>{t("productDesc")}</p>
           </div>
           <Image
             src={shopping}
@@ -27,13 +24,8 @@ const Add = () => {
         </Link>
         <Link href="/add/campsite">
           <div>
-            <h2>საპიკნიკე ადგილის გაქირავება</h2>
-            <p>
-              აქვს თქვენ სახლს ეზო და გსურთ მიიღოთ დამატებითი შემოსავალი? მაშინ
-              განათავსეთ თქვენი სივრცე დღიურად გასაქირავებლად, სადაც მსურველებს
-              შეეძლებათ დაბანაკდნენ, გაშალონ კარვები, დააყენონ მანქანები და
-              მოიწყონ პიკნიკი.
-            </p>
+            <h2>{t("campsite")}</h2>
+            <p>{t("campsiteDesc")}</p>
             <Image
               src={camping}
               alt="camping"
