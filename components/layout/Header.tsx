d6e110a3cb2e9795e607b46d4e462c5e5f3810f2
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-21 03:13:50 +0400

    add sticky header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -10,48 +10,44 @@ function Header() {
   const locale = useCurrentLocale();
 
   return (
-    <>
-      <header>
-        <nav>
-          <div className="header-desktop-nav">
-            <ul>
-              <li className={path == `/${locale}` ? "choosen-header-nav" : ""}>
-                <Link href={`/${locale}/`}>{t("home")}</Link>
-              </li>
-              <li
-                className={
-                  path == `/${locale}/products` ? "choosen-header-nav" : ""
-                }
-              >
-                <Link href={`/${locale}/products`}>{t("products")}</Link>
-              </li>
-              <li
-                className={
-                  path == `/${locale}/campsites` ? "choosen-header-nav" : ""
-                }
-              >
-                <Link href={`/${locale}/campsites`}>{t("campsites")}</Link>
-              </li>
-              <li
-                className={
-                  path == `/${locale}/blog` ? "choosen-header-nav" : ""
-                }
-              >
-                <Link href={`/${locale}/blog`}>{t("blog")}</Link>
-              </li>
-              <li
-                className={
-                  path == `/${locale}/contact` ? "choosen-header-nav" : ""
-                }
-              >
-                <Link href={`/${locale}/contact`}>{t("contact")}</Link>
-              </li>
-            </ul>
-          </div>
-          <HamburgerMenu />
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav>
+        <div className="header-desktop-nav">
+          <ul>
+            <li className={path == `/${locale}` ? "choosen-header-nav" : ""}>
+              <Link href={`/${locale}/`}>{t("home")}</Link>
+            </li>
+            <li
+              className={
+                path == `/${locale}/products` ? "choosen-header-nav" : ""
+              }
+            >
+              <Link href={`/${locale}/products`}>{t("products")}</Link>
+            </li>
+            <li
+              className={
+                path == `/${locale}/campsites` ? "choosen-header-nav" : ""
+              }
+            >
+              <Link href={`/${locale}/campsites`}>{t("campsites")}</Link>
+            </li>
+            <li
+              className={path == `/${locale}/blog` ? "choosen-header-nav" : ""}
+            >
+              <Link href={`/${locale}/blog`}>{t("blog")}</Link>
+            </li>
+            <li
+              className={
+                path == `/${locale}/contact` ? "choosen-header-nav" : ""
+              }
+            >
+              <Link href={`/${locale}/contact`}>{t("contact")}</Link>
+            </li>
+          </ul>
+        </div>
+        <HamburgerMenu />
+      </nav>
+    </header>
   );
 }
 
