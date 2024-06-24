dfa6a025839eb4b30e6f99a0cfdea68a8f728d13
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-06-24 13:32:15 +0400

    update profile navigation

--- components/Profile/profileNavigation.tsx
+++ components/Profile/profileNavigation.tsx
@@ -1,23 +1,44 @@
 import { Link } from "next-view-transitions";
-import { useScopedI18n } from "../../locales/client";
+import { useCurrentLocale, useScopedI18n } from "../../locales/client";
+import { usePathname } from "next/navigation";
 
 const ProfileNavigation = () => {
   const t = useScopedI18n("profile");
+  const path = usePathname();
+  const locale = useCurrentLocale();
   return (
     <div className="profile-user">
       <h2 className="profile-navigation-head">{t("navigation")}</h2>
       <div>
-        <Link href="/profile" className="profile-navigation">
+        <Link
+          href="/profile"
+          className={
+            path == `/${locale}/profile`
+              ? "profile-navigation bg-gray-300"
+              : "profile-navigation"
+          }
+        >
           {t("myInfo")}
         </Link>
-        <Link href="/profile/address" className="profile-navigation">
+        <Link
+          href="/profile/address"
+          className={
+            path == `/${locale}/profile/address`
+              ? "profile-navigation bg-gray-300"
+              : "profile-navigation"
+          }
+        >
           {t("myAddy")}
         </Link>
-        <Link href="/profile/orders" className="profile-navigation">
+        <Link
+          href="/profile/orders"
+          className={
+            path == `/${locale}/profile/orders`
+              ? "profile-navigation bg-gray-300"
+              : "profile-navigation"
+          }
+        >
           {t("myOrders")}
-        </Link>
-        <Link href="" className="profile-navigation">
-          {t("myPlans")}
         </Link>
       </div>
     </div>
